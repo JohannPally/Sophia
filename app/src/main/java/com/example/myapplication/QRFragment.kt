3f985d54faae5055df4405c254c8689f39ef129f
MantejSingh21 <50661183+MantejSingh21@users.noreply.github.com>
2021-10-13 22:18:53 -0500

    Added:
    1. Camera permission
    2. Add QR code library dependencies
    3. Added xml layout for QR scanner
    4. Added kt file to handle qr logic
    
    NEXT STEPS:
    1. Figure out where the result of the qr read is being stored and save that as a variable we can manipulate.

--- app/src/main/java/com/example/myapplication/QRFragment.kt
+++ app/src/main/java/com/example/myapplication/QRFragment.kt
@@ -1,22 +1,30 @@
 package com.example.myapplication;
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
+import com.budiyev.android.codescanner.*
+import com.budiyev.android.codescanner.CodeScanner
 import com.example.myapplication.databinding.FragmentQrBinding
-import androidx.navigation.fragment.navArgs
+import java.util.jar.Manifest
 
 class QRFragment : Fragment() {
     private var _binding: FragmentQrBinding? = null
     private val binding get() = _binding!!
-
     private var ctrl: DBController ? = null
+
+    private lateinit var codeScanner : CodeScanner
+    private lateinit var scannerView: CodeScannerView
 
     // Reference to the front end model that handles navigation from screen to screen
     private val navMod: NavMod by activityViewModels()
@@ -32,7 +40,6 @@ class QRFragment : Fragment() {
     }
 
     //val args: QRFragmentArgs by navArgs()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -42,10 +49,66 @@ class QRFragment : Fragment() {
             println("Failed to cast activity as MainActivity in QR Fragment")
         }
 
+        scannerView = view.findViewById(R.id.scanner_view)
+
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(context as Context , android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(activity as Activity, arrayOf(android.Manifest.permission.CAMERA), 123)
+        } else {
+            startScanning()
+        }
+
+
         //========================BINDINGS====================================
 
 
+
     }
+
+    private fun startScanning() {
+        // Parameters (default values)
+        codeScanner = CodeScanner(context as Context, scannerView) // TODO sus
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            activity?.runOnUiThread {
+                Toast.makeText(context as Context, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            activity?.runOnUiThread {
+                Toast.makeText(context as Context , "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG).show() // TODO sus context as context
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(::codeScanner.isInitialized) {
+            codeScanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if(::codeScanner.isInitialized) {
+            codeScanner?.releaseResources()
+        }
+        super.onPause()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
